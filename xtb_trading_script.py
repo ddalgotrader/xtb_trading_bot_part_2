a76ff5ad246cb69a8de9c9ce69d97954228ec6b9
Slawomir Lisowski <slisowski@gmail.com>
2024-04-05 06:35:42 +0200

    add comments

--- xtb_trading_script.py
+++ xtb_trading_script.py
@@ -10,50 +10,70 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
 
+#instantiate trading session
+xt = XtbTrader(instrument='EURUSD', interval='1min', lookback=100, strategy=contrarian,
+               units=0.1, duration=0.5, csv_results_path='gs://trading-bot-xtb-bucket')
 
-xt = XtbTrader(instrument='EURUSD', interval='1min', lookback=1000, strategy=contrarian,
-               units=0.1, duration=2, csv_results_path='gs://trading-bot-xtb-bucket')
-
+#connect to XTB API Client
 client = APIClient()
 resp = client.execute(loginCommand(user_id, pwd))
 xt.get_last_n_candles(client)
 xt.client=client
 ssid = resp['streamSessionId']
+
+#connect to XTB API Stream Client
 sclient = APIStreamClient(ssId=ssid, candleFun=xt.procCandle, aliveFun=xt.collectAlive)
+
+#function that sends 'KEEP ALIVE' message every 3 seconds with current timestamp
 sclient.subscribeAlive()
+
+#stream of candles every 1 minute
 sclient.subscribeCandle('EURUSD')
 
 try:
     while True:
 
-
+        #current duration of trading session
         session_lasting = (datetime.datetime.now() - xt.session_start).total_seconds()
-
+	
+	
+	#ping to XTB API every 5 minutes	
         if int(session_lasting)%300==0:
             client.commandExecute('ping')
             sclient.execute(dict(command='ping', streamSessionId=ssid))
 
        
-
+	#last_signal is timestamp from 'KEEP ALIVE' message if last message is older than 60 seconds from now session is closed due to not responding API
         if xt.last_signal!=None:
             if ((datetime.datetime.now()-xt.last_signal).total_seconds())>60:
-                print(xt.last_signal)
+                
                 xt.close_session(session_dead=True)
-
+		sclient.unsubscribeCandle('EURUSD')
+            	sclient.unsubscribeAlive()
+            	client.disconnect()
+            	print(f'Session terminated due to not responding API last keep alive signal -{xt.last_signal}')
+            	os.system(f'tmux capture-pane -pS - > {xt.strategy.__name__}_{xt.instrument}_{xt.interval}_{xt.end_to_file_name}.log')
+            	
                 break
+                
+        #if current duration of session is longer than duration defined in xt session is finished, logs are saved in file on the disk        
         if session_lasting >= datetime.timedelta(hours=xt.duration).total_seconds():
+           
+            xt.close_session()
             sclient.unsubscribeCandle('EURUSD')
             sclient.unsubscribeAlive()
-            xt.close_session()
-            
             client.disconnect()
             os.system(f'tmux capture-pane -pS - > {xt.strategy.__name__}_{xt.instrument}_{xt.interval}_{xt.end_to_file_name}.log')
 
             break
 
+
+# if session is closed by press CTRL+C, behaviour is the same like in finished session
 except KeyboardInterrupt:
 
-    xt.close_session()
-    client.disconnect()
-    os.system(f'tmux capture-pane -pS - > {xt.strategy.__name__}_{xt.instrument}_{xt.interval}_{xt.end_to_file_name}.log')
+     xt.close_session()
+     sclient.unsubscribeCandle('EURUSD')
+     sclient.unsubscribeAlive()
+     client.disconnect()
+     os.system(f'tmux capture-pane -pS - > {xt.strategy.__name__}_{xt.instrument}_{xt.interval}_{xt.end_to_file_name}.log')
 
